6d2bbd03b19141cda7a0e769824a3c0480e5ae88
Antonio le Caldare <antonio.lecaldare@gmail.com>
2017-11-03 00:46:26 +0100

    aggiunta classe keyboard (eredita IODevice). Tutti i metodi sono protetti da mutex globale, possibilità di abitazione e disabilitazione, supporto al solo polling

---
+++ frontend/keyboard.cpp
@@ -0,0 +1,68 @@
+#include "keyboard.h"
+
+keyboard::keyboard() : RBR(0), TBR(0), STR(0), CMR(0), enabled(false), interrupt_enabled(false), interrupt_raised(false) {
+	pthread_mutex_init(&mutex, NULL);
+}
+
+void keyboard::write_reg_byte(io_addr addr, uint8_t val)
+{
+	pthread_mutex_lock(&mutex);
+
+	switch(addr) {
+		case CMR_addr: CMR=val; break;
+		case TBR_addr: TBR=val; process_cmd(); break;
+	}
+
+	pthread_mutex_unlock(&mutex);
+}
+
+uint8_t keyboard::read_reg_byte(io_addr addr)
+{
+	pthread_mutex_lock(&mutex);
+
+	switch(addr) {
+		case RBR_addr:
+			STR &= ~FI_MASK;
+			interrupt_raised = false;
+			return RBR;
+		case STR_addr: return STR;
+	}
+
+	pthread_mutex_unlock(&mutex);
+}
+
+void keyboard::process_cmd()
+{
+	switch(CMR) {
+		// === configurazione tastiera ===
+		case 0x60:
+			if(TBR & 0x10)	// bit 4: abilitazione tastiera
+				enabled = true;
+			else
+				enabled = false;
+
+			if(TBR & 0x01)	// bit 0: abilitazione interruzioni
+				interrupt_enabled = true;
+			else
+				interrupt_enabled = false;
+		break;
+	}
+}
+
+void keyboard::insert_keycode_event(uint8_t keycode)
+{
+	pthread_mutex_lock(&mutex);
+
+	if(!enabled)
+		return;
+
+	// mettiamo il keycode nel registro RBR e segnaliamo che il buffer è pieno
+	RBR = keycode;
+	STR |= FI_MASK;
+
+	// provochiamo un'interruzione
+	if(interrupt_enabled)
+		interrupt_raised = true;
+
+	pthread_mutex_unlock(&mutex);
+}
---
+++ frontend/keyboard.h
@@ -0,0 +1,43 @@
+#include "IODevice.h"
+#include <stdint.h>
+#include <pthread.h>
+
+class keyboard : public IODevice {
+private:
+	// === Registri ===
+	uint8_t RBR;	// buffer di ingresso
+	uint8_t TBR;	// buffer di uscita
+	uint8_t STR;	// registro di stato
+	uint8_t CMR;	// registro di comando
+
+	// indirizzi registri
+	static const io_addr RBR_addr = 0x0060;
+	static const io_addr TBR_addr = 0x0060;
+	static const io_addr STR_addr = 0x0064;
+	static const io_addr CMR_addr = 0x0064;
+
+	// flag per registro STR
+	static const uint8_t FI_MASK = 1u;
+	static const uint8_t FO_MASK = 1u << 1;
+
+	// === Stato interno ===
+	bool enabled;
+	bool interrupt_enabled;
+
+	bool interrupt_raised;
+
+	// mutex istanza (vale sia per frontend che backend)
+	pthread_mutex_t mutex;
+
+private:
+	void process_cmd();
+
+public:
+	keyboard();
+
+	void write_reg_byte(io_addr addr, uint8_t val);
+	uint8_t read_reg_byte(io_addr addr);
+
+	// serve al backend per pushare eventi di inserimento come handler
+	void insert_keycode_event(uint8_t keycode);
+};
