f5b73ce8b6ab16bfa09ea83fb23a7b0bbccb0ffa
Antonio le Caldare <antonio.lecaldare@gmail.com>
2017-11-12 02:54:04 +0100

    kvm: stampa trace programma target su Triple Fault

--- kvm.cpp
+++ kvm.cpp
@@ -303,6 +303,7 @@ int main(int argc, char **argv)
 				break;
 			case KVM_EXIT_SHUTDOWN:
 				cerr << "kvm: TRIPLE FAULT. Shutting down" << endl;
+				trace_user_program(vcpu_fd, kr);
 				return 1;
 			case KVM_EXIT_FAIL_ENTRY:
 				cerr << "kvm: KVM_EXIT_FAIL_ENTRY reason=" << (unsigned long long)kr->fail_entry.hardware_entry_failure_reason << endl;
